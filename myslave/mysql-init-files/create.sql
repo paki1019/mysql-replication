37dc0640283dc5e545884ac4c37fd726607e162e
paki1019 <paki1019@gmail.com>
2021-04-25 13:45:08 +0900

    mysql-replication

---
+++ myslave/mysql-init-files/create.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE mydb;
+
+CREATE TABLE mydb.user(
+  id VARCHAR(100) PRIMARY KEY,
+  name VARCHAR (100),
+  age INT
+);
+
+#create masteruser and grant privileges
+create user slaveuser@'%' identified by 'slavepassword';
+grant all privileges on mydb.* to slaveuser@'%' identified by 'slavepassword';
